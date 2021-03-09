6571e3956eed1cbfe1f66d7cc0cb5ea6c68cda0f
Fiki Maulana <fikimaul@gmail.com>
2021-03-09 10:33:12 +0700

    Initial Release

---
+++ README.md
@@ -0,0 +1,16 @@
+# whatsapp-online-tracker
+Sebuah Ekstensi Google Chrome untuk tracking status online kontak WhatsApp.
+
+## Fitur:
+  - Traking kapan kontak mulai dan selesai serta info durasi online.
+  - Export hasil tracking ke CSV.
+  
+## Steps to Install:
+  - [download](https://github.com/fikimaul/whatsapp-online-tracking/archive/master.zip) dan ekstrak arsip.
+  - Setelah diekstrak akan mendapatkan forder bernama whatsapp-online-tracking.
+  - Buka [chrome://extensions](chrome://extensions) di browser berbasis chromium  yang mendukung Ekstensi Google Chrome.
+  - Pastikan developer mode hidup.
+  - Klik 'Load unpacked' dan pilih folder yang telah diekstrak.
+  - Buka whatsapp web dan klik ikon ekstensi.
+  - Klik 'Start' untuk mulai Traking.
+  - Pilih Kontak yang akan ditraking status onlinenya.
---
+++ track.js
@@ -0,0 +1,71 @@
+var csvContent = "data:text/csv;charset=utf-8,Name,Date,From,To,Duration\n";
+var state = "offline";
+var currName =  "";
+var startTime;
+var endTime;
+
+console.log("Mulai");
+
+function GetOnline(){
+   let main = document.getElementById("main");
+   if(main != null || main != undefined){
+      let name  = main.childNodes[1].childNodes[1].childNodes[0].childNodes[0].childNodes[0].innerText;
+      
+      if(name != currName){
+         state = "offline";
+         currName = name;   
+      }
+
+      let objOnline = main.querySelectorAll('span[title="online"]');
+      let objTyping = main.querySelectorAll('span[title="typing…"]')
+     
+      if((objOnline.length != 0 || objTyping.length != 0) && state == "offline"){
+         state = "online";
+         startTime = new Date().getTime();
+         console.log("=== "+name+" === ");
+         
+         new Notification(`${name} Online`, {
+            icon: 'https://img.icons8.com/ios-filled/50/000000/whatsapp.png',
+			body: `Time: ${DateFormat(startTime)}\n`
+         });
+        
+         console.log(">> Mulai Online     : "+DateFormat(startTime));
+      }else if((objOnline.length == 0 && objTyping.length == 0)  && state == "online" ){
+         state = "offline";
+         endTime = new Date().getTime();
+         
+         let diff = parseInt((endTime-startTime)/1000);
+         let duration = `${parseInt(diff/60)} Menit ${diff%60} Detik`;
+         
+         csvContent += `${name},${new Date(startTime).toLocaleDateString()},${new Date(startTime).toLocaleTimeString()},${new Date(endTime).toLocaleTimeString()},${duration}\n`;
+         
+         new Notification(`${name} Offline`, {
+            icon: 'https://img.icons8.com/ios-filled/50/000000/whatsapp.png',
+			body: `Dari ${new Date(startTime).toLocaleDateString()},  ${new Date(startTime).toLocaleTimeString()} sampai ${new Date(endTime).toLocaleTimeString()}\n Durasi: ${duration}`
+         });
+                
+         console.log(">> Terakhir Online  : "+DateFormat(endTime)+", Durasi "+duration);
+         console.log("===================")
+      }
+   }
+}
+
+var intervalID = setInterval(function(){GetOnline()}, 1000);
+
+function StopMonitor(){
+   console.log("Selesai");
+   clearInterval(intervalID);
+}
+
+function DateFormat(date){
+    return new Date(date).toLocaleDateString()+" "+new Date(date).toLocaleTimeString()
+}
+
+function DownloadCSV() {
+	data = encodeURI(csvContent);
+	let link = document.createElement("a");
+	link.setAttribute("href", data);
+	link.setAttribute("download", "stalk_data.csv");
+	document.body.appendChild(link);
+	link.click();
+}
